80022137d3b2431ad877c66d26ddf3daf9026c1a
ZergOfSwarm <d358468115905@gmail.com>
2020-02-26 11:47:29 +0200

    Endless ping OpenVPN + MQTT

--- ping.sh
+++ ping.sh
@@ -1,40 +1,43 @@
 #!/bin/bash
-#LOGFILE="/home/denis/result_of_ping"
-IPS='10.19.0.1' # Здесь можешь перечислить список IP адресов каждый ip с новой строчки или через пробел!
+#LOGFILE="/home/denis/result_of_ping" # Раскомментировать если нужно вести log.file
+IPS='10.19.0.1' # Здесь можешь перечислить список "IP" адресов каждый "ip" с новой строчки или через пробел!
+mqtt_server='192.168.18.12' # Здесь пропишем "IP" адрес "MQTT" сервера.
 
-function zerg_ping (){
+function zerg_ping (){ # Функция "zerg_ping" которая пингует и результат пишет либо в лог.файл или отправляет на MQTT.
 for IP_ADDRESS in ${IPS}; do
    echo \n 
    echo "TEST FOR ${IP_ADDRESS}"
-   ping -q -c2 ${IP_ADDRESS} > /dev/null
+   ping -q -c2 ${IP_ADDRESS} > /dev/null # Заскомментировать эту строку если нужно вести log.file
+   #ping -q -c2 ${IP_ADDRESS} && mosquitto_pub -h localhost -t "device/"$network$number -m "1" (Раскомментировать если нужно вести log.file)
    if [ $? -eq 0 ] 
    then
-   #echo date=$(date '+%Y-%m-%d %H:%M:%S') >> $LOGFILE 2>&1 # Пишем в лог файл "IP" если пингуется, дату и время.
-   #echo ${IP_ADDRESS} "Pingable" >> $LOGFILE 2>&1 # Пишем в лог файл "Pingable"
-   echo ${IP_ADDRESS} "Pingable"
+   #echo date=$(date '+%Y-%m-%d %H:%M:%S') >> $LOGFILE 2>&1 # Пишем в лог файл дату и время если не пингуется. (Раскомментировать если нужно вести log.file)
+   #echo ${IP_ADDRESS} "Pingable" >> $LOGFILE 2>&1 # Пишем в лог файл "IP" и слово "Pingable". (Раскомментировать если нужно вести log.file)
+   echo ${IP_ADDRESS} "Pingable" && mosquitto_pub -h $mqtt_server -t "IP-"$IPS -m "1" # Если пингуется пишем "1" в mqtt.
    else
-   #echo date=$(date '+%Y-%m-%d %H:%M:%S') >> $LOGFILE 2>&1 # Пишем в лог файл "IP" если не пингуется, дату и время.
-   #echo ${IP_ADDRESS} "Not Pingable" >> $LOGFILE 2>&1 # Пишем в лог файл "Not Pingable"
-   echo ${IP_ADDRESS} "Not Pingable"
-   #echo "Killing all opened  openVPN services!"
+   #echo date=$(date '+%Y-%m-%d %H:%M:%S') >> $LOGFILE 2>&1 # Пишем в лог файл дату и время если не пингуется. (Раскомментировать если нужно вести log.file)
+   #echo ${IP_ADDRESS} "Not Pingable" >> $LOGFILE 2>&1 # Пишем в лог файл "IP" и слово "Not Pingable" (Раскомментировать если нужно вести log.file)
+   echo ${IP_ADDRESS} "Not Pingable" && mosquitto_pub -h $mqtt_server -t "IP-"$IPS -m "0" # Если не пингуется пишем "0" в mqtt.
+   # Перед запуском "OpenVPN" убиваем все запущенные сервесы с "OpenVPN".
    PID=`ps -eaf | grep openvpn | grep -v grep | awk '{print $2}'`
    if [[ "" !=  "$PID" ]]; then
    echo "killing $PID"
    kill -9 $PID
    fi
-   #sleep 5s  # Pause 5 seconds
+   # Не уверен, что здесь пауза нужна ну, поставил на всякий случай!
+   #sleep 5s
    read -p 'Pausing for 5 seconds' -t 5
    echo \n
-   echo "Runing OpenVPN!"
+   echo "Runing OpenVPN."
    openvpn --config /home/denis/.ovpn/Synology/VPNConfig.ovpn &
    fi
 
 done
 }
 
-while true; do
+while true; do # Бесконечный цикл
    #sleep 5s 
-   read -p 'Runing my function after pause in 5 seconds' -t 5
+   read -p 'Runing "zerg_ping" function after pause in 5 seconds' -t 5
    echo \n
    echo "Run my function zerg_ping"
    zerg_ping
